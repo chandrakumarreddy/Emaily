859f8ffad217f0a92d32f225febb202e64cd9ffe
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-05-19 02:38:56 +0530

    file removed

--- config/dev_keys.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-	'googleClientID':'625953240666-m8rj32hfdp4m57s4boc4rr4lts0o3175.apps.googleusercontent.com',
-	'googleSecret':'_UrcF19shG5zBq-aFHBzj4g_',
-	'monogoClient':'mongodb://chandrareddy:Chandra123@ds227740.mlab.com:27740/emaily_dev',
-	'cookie_key':'dshjkfhabvjkdsbavjkdbvjksdbfgjksghfajdkbvjdfss'
-}
